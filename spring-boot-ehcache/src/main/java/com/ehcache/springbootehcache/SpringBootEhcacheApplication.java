63198f2a608fc13a8775c2d267065e195204fd96
brokencolor <huajianzhiyue@sina.com>
2019-05-10 11:47:04 +0800

    创建ehcache测试项目

---
+++ spring-boot-ehcache/src/main/java/com/ehcache/springbootehcache/SpringBootEhcacheApplication.java
@@ -0,0 +1,13 @@
+package com.ehcache.springbootehcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEhcacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEhcacheApplication.class, args);
+    }
+
+}
