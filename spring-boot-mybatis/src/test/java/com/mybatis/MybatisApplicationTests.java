1a2317bd75a6872c09ed20946921f4572e044bb3
brokencolor <huajianzhiyue@sina.com>
2019-05-21 19:52:56 +0800

    spring-boot-mybaits 修改配置

--- spring-boot-mybatis/src/test/java/com/mybatis/MybatisApplicationTests.java
+++ spring-boot-mybatis/src/test/java/com/mybatis/MybatisApplicationTests.java
@@ -1,11 +1,14 @@
 package com.mybatis;
 
+import com.mybatis.mapper.master.UserMapper;
 import com.mybatis.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -14,10 +17,14 @@ public class MybatisApplicationTests {
 	@Autowired
 	private UserService userService;
 
+	@Resource
+	private UserMapper umapper;
+
 	@Test
 	public void contextLoads() {
 
-		this.userService.getUserById(8L);
+		System.out.println(this.umapper.getUserById(8L));
+		System.out.println(this.userService.getUserById(8L));
 	}
 
 }
