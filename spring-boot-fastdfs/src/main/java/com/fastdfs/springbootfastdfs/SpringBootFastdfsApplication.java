7d35a337b6069ce296d024b72eeb4ce206d0fd1e
guwanli <guwanli@bonc.com.cn>
2019-11-12 20:06:51 +0800

    项目创建为子模块-添加fastdfs

---
+++ spring-boot-fastdfs/src/main/java/com/fastdfs/springbootfastdfs/SpringBootFastdfsApplication.java
@@ -0,0 +1,13 @@
+package com.fastdfs.springbootfastdfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFastdfsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFastdfsApplication.class, args);
+    }
+
+}
