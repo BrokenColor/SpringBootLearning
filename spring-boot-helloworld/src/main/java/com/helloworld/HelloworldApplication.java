570287a39c270fcd7f8e825eafe8eb2a0587c5ca
顾万里 <guwanli@bonc.com.cn>
2019-12-05 20:11:17 +0800

    添加banner

--- spring-boot-helloworld/src/main/java/com/helloworld/HelloworldApplication.java
+++ spring-boot-helloworld/src/main/java/com/helloworld/HelloworldApplication.java
@@ -1,13 +1,29 @@
 package com.helloworld;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class HelloworldApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloworldApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        // 目的是
+        return args -> {
+            System.out.println("来看看 SpringBoot 默认为我们提供的 Bean：");
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            Arrays.stream(beanNames).forEach(System.out::println);
+        };
     }
 
 }
